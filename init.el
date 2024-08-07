7f505da74ca27ab6484e3fa1df3da97334fe8bc6
Oleg Montak <oleg.montak@gmail.com>
2024-08-07 16:06:17 +0300

    add ejc-sql

--- init.el
+++ init.el
@@ -259,6 +259,15 @@
                                                            (emacs-lisp . t)
                                                            (shell . t))))
 
+;; sql
+
+(use-package ejc-sql
+  :custom
+  (ejc-jdbc-drivers
+   '("sqlite"     [org.xerial/sqlite-jdbc "3.46.0.1"]
+     "postgresql" [postgresql/postgresql "9.3-1102.jdbc41"]))
+  :after (sql-mode org-mode))
+
 ;; yaml
 
 (use-package yaml-mode
@@ -411,7 +420,7 @@ The result is returned as a string."
  '(custom-safe-themes
    '("c7a926ad0e1ca4272c90fce2e1ffa7760494083356f6bb6d72481b879afce1f2" "c1638a7061fb86be5b4347c11ccf274354c5998d52e6d8386e997b862773d1d2" default))
  '(package-selected-packages
-   '(corfu gptel ob-http ob-swift kotlin-ts-mode editorconfig copilot quelpa-use-package quelpa exec-path-from-shell languagetool markdown-mode vterm embark-consult embark orderless consult marginalia vertico modus-themes wgrep dumb-jump multiple-cursors expand-region evil-nerd-commenter magit helpful yaml-mode swift-mode)))
+   '(ejc-sql corfu gptel ob-http ob-swift kotlin-ts-mode editorconfig copilot quelpa-use-package quelpa exec-path-from-shell languagetool markdown-mode vterm embark-consult embark orderless consult marginalia vertico modus-themes wgrep dumb-jump multiple-cursors expand-region evil-nerd-commenter magit helpful yaml-mode swift-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
