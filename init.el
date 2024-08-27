219baa8f3bda5143723823e7dd53f9de6bfbef94
Oleg Montak <oleg.montak@gmail.com>
2024-08-27 14:49:33 +0300

    add magit-forge

--- init.el
+++ init.el
@@ -313,12 +313,17 @@
 
 ;; magit
 
+(setq auth-sources '("~/.authinfo"))
+
 (use-package magit
   :commands (magit-status)
   :custom
   (magit-display-buffer-function 'magit-display-buffer-same-window-except-diff-v1)
   (magit-define-global-key-bindings 'recommended)
   (magit-diff-refine-hunk 'all))
+
+(use-package forge
+  :after magit)
 
 ;; evil-nerd-commenter
 
@@ -444,7 +449,7 @@ The result is returned as a string."
  '(custom-safe-themes
    '("c7a926ad0e1ca4272c90fce2e1ffa7760494083356f6bb6d72481b879afce1f2" "c1638a7061fb86be5b4347c11ccf274354c5998d52e6d8386e997b862773d1d2" default))
  '(package-selected-packages
-   '(cape ob-swiftui ejc-sql corfu gptel ob-http ob-swift kotlin-ts-mode editorconfig copilot quelpa-use-package quelpa exec-path-from-shell languagetool markdown-mode vterm embark-consult embark orderless consult marginalia vertico modus-themes wgrep dumb-jump multiple-cursors expand-region evil-nerd-commenter magit helpful yaml-mode swift-mode)))
+   '(forge cape ob-swiftui ejc-sql corfu gptel ob-http ob-swift kotlin-ts-mode editorconfig copilot quelpa-use-package quelpa exec-path-from-shell languagetool markdown-mode vterm embark-consult embark orderless consult marginalia vertico modus-themes wgrep dumb-jump multiple-cursors expand-region evil-nerd-commenter magit helpful yaml-mode swift-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
