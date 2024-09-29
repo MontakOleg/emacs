392e385c0720528dbe1a1482d447d29dcf6774f2
Oleg Montak <oleg.montak@gmail.com>
2024-09-29 13:47:45 +0300

    add explicit nil for vertical-scroll-bars

--- early-init.el
+++ early-init.el
@@ -10,7 +10,7 @@
 ;; Faster to disable these here (before they've been initialized)
 (push '(menu-bar-lines . 0) default-frame-alist)
 (push '(tool-bar-lines . 0) default-frame-alist)
-(push '(vertical-scroll-bars) default-frame-alist)
+(push '(vertical-scroll-bars . nil) default-frame-alist)
 
 ;; Resizing the Emacs frame can be a terribly expensive part of changing the
 ;; font. By inhibiting this, we easily halve startup times with fonts that are
