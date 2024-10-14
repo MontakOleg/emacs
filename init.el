bc3c9e704c5d587102986ffcccf9e76d6606fff3
Oleg Montak <oleg.montak@gmail.com>
2024-10-14 16:00:30 +0300

    add restclient

--- init.el
+++ init.el
@@ -210,6 +210,9 @@
 
 (recentf-mode 1)
 
+;; restclient
+(use-package restclient)
+
 ;; languagetool
 
 (use-package languagetool
@@ -512,7 +515,7 @@ The result is returned as a string."
  '(custom-safe-themes
    '("c7a926ad0e1ca4272c90fce2e1ffa7760494083356f6bb6d72481b879afce1f2" "c1638a7061fb86be5b4347c11ccf274354c5998d52e6d8386e997b862773d1d2" default))
  '(package-selected-packages
-   '(go-mode rainbow-mode git-modes forge cape ob-swiftui ejc-sql corfu gptel ob-http ob-swift kotlin-ts-mode editorconfig copilot quelpa-use-package quelpa exec-path-from-shell languagetool markdown-mode vterm embark-consult embark orderless consult marginalia vertico modus-themes wgrep dumb-jump multiple-cursors expand-region evil-nerd-commenter magit helpful yaml-mode swift-mode)))
+   '(restclient go-mode rainbow-mode git-modes forge cape ob-swiftui ejc-sql corfu gptel ob-http ob-swift kotlin-ts-mode editorconfig copilot quelpa-use-package quelpa exec-path-from-shell languagetool markdown-mode vterm embark-consult embark orderless consult marginalia vertico modus-themes wgrep dumb-jump multiple-cursors expand-region evil-nerd-commenter magit helpful yaml-mode swift-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
