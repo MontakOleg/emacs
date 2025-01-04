39ba7412f68e702f80ba67840d8ec86e95985927
Oleg Montak <oleg.montak@gmail.com>
2025-01-05 01:02:31 +0200

    diminish editorconfig

--- init.el
+++ init.el
@@ -83,7 +83,10 @@
 
 ;; EditorConfig
 
+(use-package diminish)
+
 (use-package editorconfig
+  :diminish editorconfig-mode
   :config
   (editorconfig-mode 1))
 
@@ -566,7 +569,7 @@ The result is returned as a string."
  '(custom-safe-themes
    '("c7a926ad0e1ca4272c90fce2e1ffa7760494083356f6bb6d72481b879afce1f2" "c1638a7061fb86be5b4347c11ccf274354c5998d52e6d8386e997b862773d1d2" default))
  '(package-selected-packages
-   '(move-text eglot kotlin-ts-mode jinx restclient go-mode rainbow-mode git-modes forge cape ejc-sql corfu gptel ob-http ob-swift editorconfig copilot quelpa-use-package quelpa exec-path-from-shell languagetool markdown-mode vterm embark-consult embark orderless consult marginalia vertico modus-themes wgrep dumb-jump multiple-cursors expand-region evil-nerd-commenter magit helpful yaml-mode swift-mode)))
+   '(diminish move-text eglot kotlin-ts-mode jinx restclient go-mode rainbow-mode git-modes forge cape ejc-sql corfu gptel ob-http ob-swift editorconfig copilot quelpa-use-package quelpa exec-path-from-shell languagetool markdown-mode vterm embark-consult embark orderless consult marginalia vertico modus-themes wgrep dumb-jump multiple-cursors expand-region evil-nerd-commenter magit helpful yaml-mode swift-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
